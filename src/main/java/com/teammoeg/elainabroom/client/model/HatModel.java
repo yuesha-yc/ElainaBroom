fb2ef0b436efac3795a92213ac496cb8a741d191
yuesha-yc <kevinwangyichen@gmail.com>
2020-11-21 16:53:47 +0800

    Hat model Improved

--- src/main/java/com/teammoeg/elainabroom/client/model/HatModel.java
+++ src/main/java/com/teammoeg/elainabroom/client/model/HatModel.java
@@ -11,12 +11,8 @@ public class HatModel extends EntityModel<AbstractClientPlayerEntity> implements
 
     private final ModelPart hattop;
     private final ModelPart hatring;
-    private final ModelPart a6_r1;
-    private final ModelPart a5_r1;
     private final ModelPart a4_r1;
     private final ModelPart a3_r1;
-    private final ModelPart a2_r1;
-    private final ModelPart a1_r1;
 
     public HatModel(float scale) {
         this(scale, 128, 128);
@@ -26,51 +22,33 @@ public class HatModel extends EntityModel<AbstractClientPlayerEntity> implements
         textureWidth = u;
         textureHeight = v;
 
+        textureWidth = 128;
+        textureHeight = 128;
+
         hattop = new ModelPart(this);
-        hattop.setPivot(0.0F, 24.0F, 0.0F);
-        hattop.setTextureOffset(44, 45).addCuboid(-5.0F, -39.0F, -2.0F, 6.0F, 3.0F, 6.0F, 0.0F, false);
-        hattop.setTextureOffset(29, 0).addCuboid(-5.0F, -33.0F, -4.0F, 8.0F, 3.0F, 8.0F, 0.0F, false);
-        hattop.setTextureOffset(26, 41).addCuboid(-4.0F, -37.0F, -3.0F, 6.0F, 4.0F, 6.0F, 0.0F, false);
+        hattop.setPivot(-1.3333F, -12.3333F, 0.3333F);
+        hattop.setTextureOffset(44, 45).addCuboid(-3.6667F, -4.3333F, -2.3333F, 6.0F, 3.0F, 6.0F, 0.0F, false);
+        hattop.setTextureOffset(29, 0).addCuboid(-3.6667F, 1.6667F, -4.3333F, 8.0F, 3.0F, 8.0F, 0.0F, false);
+        hattop.setTextureOffset(26, 41).addCuboid(-2.6667F, -2.3333F, -3.3333F, 6.0F, 4.0F, 6.0F, 0.0F, false);
 
         hatring = new ModelPart(this);
-        hatring.setPivot(0.0F, 24.0F, 0.0F);
-
-
-        a6_r1 = new ModelPart(this);
-        a6_r1.setPivot(1.0F, -10.0F, -8.0F);
-        hatring.addChild(a6_r1);
-        setRotationAngle(a6_r1, 0.0F, 0.0F, 0.0436F);
-        a6_r1.setTextureOffset(0, 24).addCuboid(-1.0F, -21.0F, -3.0F, 9.0F, 1.0F, 11.0F, 0.0F, false);
-
-        a5_r1 = new ModelPart(this);
-        a5_r1.setPivot(2.0F, -10.0F, 0.0F);
-        hatring.addChild(a5_r1);
-        setRotationAngle(a5_r1, 0.0F, 0.0F, -0.0436F);
-        a5_r1.setTextureOffset(0, 0).addCuboid(-1.0F, -21.0F, 0.0F, 9.0F, 1.0F, 11.0F, 0.0F, false);
+        hatring.setPivot(0.3333F, -8.3333F, 0.6667F);
+        hatring.setTextureOffset(29, 29).addCuboid(-11.1806F, -0.1621F, -1.6667F, 9.0F, 1.0F, 11.0F, 0.0F, false);
+        hatring.setTextureOffset(0, 12).addCuboid(-10.4794F, -0.4674F, -12.6667F, 9.0F, 1.0F, 11.0F, 0.0F, false);
+        hatring.setTextureOffset(0, 0).addCuboid(-0.1872F, -0.4674F, -1.6667F, 9.0F, 1.0F, 11.0F, 0.0F, false);
+        hatring.setTextureOffset(0, 24).addCuboid(0.5139F, -0.1621F, -12.6667F, 9.0F, 1.0F, 11.0F, 0.0F, false);
 
         a4_r1 = new ModelPart(this);
-        a4_r1.setPivot(3.0F, -10.0F, 7.0F);
+        a4_r1.setPivot(-1.3333F, -0.3333F, 6.3333F);
         hatring.addChild(a4_r1);
         setRotationAngle(a4_r1, 0.0F, -1.5708F, 0.0F);
-        a4_r1.setTextureOffset(29, 12).addCuboid(-3.0F, -22.0F, 0.0F, 8.0F, 2.0F, 8.0F, 0.0F, false);
+        a4_r1.setTextureOffset(29, 12).addCuboid(-4.0F, -1.0F, -4.0F, 8.0F, 2.0F, 8.0F, 0.0F, false);
 
         a3_r1 = new ModelPart(this);
-        a3_r1.setPivot(3.0F, -10.0F, -5.0F);
+        a3_r1.setPivot(-1.3333F, -0.3333F, -9.6667F);
         hatring.addChild(a3_r1);
         setRotationAngle(a3_r1, 0.0F, -1.5708F, 0.0F);
-        a3_r1.setTextureOffset(0, 36).addCuboid(-7.0F, -22.0F, 0.0F, 8.0F, 2.0F, 8.0F, 0.0F, false);
-
-        a2_r1 = new ModelPart(this);
-        a2_r1.setPivot(-3.0F, -10.0F, -8.0F);
-        hatring.addChild(a2_r1);
-        setRotationAngle(a2_r1, 0.0F, 0.0F, 0.0436F);
-        a2_r1.setTextureOffset(0, 12).addCuboid(-8.0F, -21.0F, -3.0F, 9.0F, 1.0F, 11.0F, 0.0F, false);
-
-        a1_r1 = new ModelPart(this);
-        a1_r1.setPivot(-2.0F, -10.0F, 0.0F);
-        hatring.addChild(a1_r1);
-        setRotationAngle(a1_r1, 0.0F, 0.0F, -0.0436F);
-        a1_r1.setTextureOffset(29, 29).addCuboid(-8.0F, -21.0F, 0.0F, 9.0F, 1.0F, 11.0F, 0.0F, false);
+        a3_r1.setTextureOffset(0, 36).addCuboid(-4.0F, -1.0F, -4.0F, 8.0F, 2.0F, 8.0F, 0.0F, false);
     }
 
     @Override
